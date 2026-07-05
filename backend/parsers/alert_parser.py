115c363dfd0f2df7377201b7cd90d6f7eed9db31
Nefen <nam.tran15032005@hcmut.com>
2026-07-05 17:57:50 +0700

    feat: fallback if can not use AI for parser

--- backend/parsers/alert_parser.py
+++ backend/parsers/alert_parser.py
@@ -1,7 +1,7 @@
 import re 
 from .base_parser import BaseParser
 from .edge_parser import Vertex
-from database.constraints import MAPPING_ENTITIES_TYPE
+from database.constraints import MAPPING_ENTITIES_TYPE, ALL_PATTERNS, MAPPING_RELATIONSHIPS
 from iocsearcher.searcher import Searcher
 from .llm_client import LLM_Client
 import json
@@ -36,3 +36,28 @@ class AlertParser(BaseParser):
         print(f"Encoded event: {encoded_event}")
         LLM_result = LLM_Client.parse(encoded_event, entity_search)
         return LLM_result
+
+    @classmethod
+    def from_event(cls, event: dict):
+        message = event.get("message")
+        lst = []
+        time = str(event.get("timestamp"))
+        nodes= []
+        for k,v in ALL_PATTERNS.items():
+            sub_lst = []
+            list(map(lambda x: sub_lst.extend(x.findall(message)), v))
+            nodes += list_vertex(k, sub_lst)
+            lst+=sub_lst
+        edges = [{"source": s_node,
+                   "target": t_node,
+                   "type": MAPPING_RELATIONSHIPS[(s_node.type,t_node.type)] if (s_node.type, t_node.type) in MAPPING_RELATIONSHIPS.keys() else "",
+                   "time": time,
+                   }
+                   for s_node in nodes for t_node in nodes]
+        edges=[edge for edge in edges if edge["type"] != ""]
+        return cls(
+            source_type=event.get("source_type"),
+            nodes=nodes,
+            edges=edges,
+            evidence=event.get("event_id", "")
+        )
